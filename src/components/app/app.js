4439dfb9e26892e3860ee079a20f09f31125ea02
RomanBeliy <romanpeople2292@yandex.ru>
2019-03-18 22:31:21 +0200

    add people-page

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,28 +2,19 @@ import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import ItemList from '../item-list';
-import PersonDetails from '../person-details';
-
 import './app.css';
 import ErrorIndicator from "../error-indicator";
+import PeoplePage from "../people-page";
 
 export default class App extends Component {
 
     state = {
         showRandomPlanet: true,
-        selectedPerson: 2,
         hasError: false,
     };
 
     componentDidCatch(error, errorInfo) {
         this.setState({hasError: true})
-    }
-
-    onPersonSelected = (id) => {
-        this.setState({
-            selectedPerson: id,
-        })
     };
 
     render () {
@@ -40,15 +31,7 @@ export default class App extends Component {
                 >
                     Toogle Random Planet
                 </button>
-
-                <div className="row mb2">
-                    <div className="col-md-6">
-                        <ItemList onItemSelected = {this.onPersonSelected}/>
-                    </div>
-                    <div className="col-md-6">
-                        <PersonDetails personId={this.state.selectedPerson}/>
-                    </div>
-                </div>
+                <PeoplePage/>
             </div>
         );
     }
